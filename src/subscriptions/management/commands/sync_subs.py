8ba5cd4f54a701c76090842902aaa96cce99de93
Ashhad Ahmed <ashhadahmed776@outlook.com>
2024-09-19 15:54:56 +0500

    Commit: Added Subscriptions

--- src/subscriptions/management/commands/sync_subs.py
+++ src/subscriptions/management/commands/sync_subs.py
@@ -1,7 +1,11 @@
-from typing import Any
-from django.core.management.base import BaseCommand
 from subscriptions.models import Subscription
+from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
-    def handle(self, *args: Any, **options: Any) -> None:
+    
+    def handle(self, *args, **options):
         print("Hello World!")
+        qs = Subscription.objects.filter(active=True)
+        for obj in qs:
+            for group in obj.groups.all():
+                group.permissions.set(obj.permissions.all())
--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,14 +1,54 @@
 from django.db import models
 from django.contrib.auth.models import Permission, Group
-# Create your models here.
+from django.conf import settings
+from django.db.models.signals import post_save
+
+User = settings.AUTH_USER_MODEL
+
+ALLOW_CUSTOM_GROUPS = True
+
 class Subscription(models.Model):
+    active = models.BooleanField(default=True)
     name = models.CharField(max_length=255)
     groups = models.ManyToManyField(Group)
-    permissions = models.ManyToManyField(Permission, limit_choices_to={"content_type__app_label": "subscriptions"})
+    permissions = models.ManyToManyField(Permission)
+    
+    def __str__(self):
+        return f"{self.name}"
     
     class Meta:
         permissions = [
-            ("advanced", "Advaned permission"),
-            ("pro", "Pro permission"),
-            ("basic", "Basic permission"),
+            ("advanced", "Advanced Permission"),
+            ("pro", "Pro Permission"),
+            ("basic", "Basic Permission"),
         ]
+
+class UserSubscription(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    subscriptions = models.ForeignKey(Subscription, on_delete=models.SET_NULL, null=True, blank=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.subscriptions.name}"
+
+def user_sub_post_save(sender, instance, created, *args, **kwargs):
+    print(instance)
+    user_sub_instance = instance
+    user = user_sub_instance.user
+    subscription_obj = user_sub_instance.subscriptions
+    groups = subscription_obj.groups.all()
+    if not ALLOW_CUSTOM_GROUPS:
+        user.group.set(groups)
+    else:
+        subs_qs = Subscription.objects.filter(active=True).exclude(id=subscription_obj.id)
+        subs_groups = subs_qs.groups.values_list("id", flat=True)
+        subs_groups_set = set(subs_groups)
+        # group_ids = groups.values_list("id", flat=True)
+        current_groups = user.groups.all().values_list("id", flat=True)
+        group_ids_set = set(group_ids)
+        current_groups_set = set(current_groups) - subs_groups_set
+        final_group_ids = list(group_ids_set | current_groups_set)
+        user.groups.set(final_group_ids)
+    
+    
+post_save.connect(user_sub_post_save, sender=UserSubscription)
