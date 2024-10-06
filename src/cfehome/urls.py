a51fd9485c92d3b55f3617f7626f90cda73db929
Ashhad Ahmed <ashhadahmed776@outlook.com>
2024-10-06 13:25:11 +0500

    Commit: Added better landing page

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -14,30 +14,54 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
-from checkouts.views import checkout_redirect_view, checkout_finalised_view, product_price_redirect_views
-from .views import home_view, about_view, pw_protected_view, user_only_view, staff_only_view
+from checkouts.views import (
+    checkout_redirect_view,
+    checkout_finalised_view,
+    product_price_redirect_views,
+)
+from .views import (
+    home_view,
+    about_view,
+    pw_protected_view,
+    user_only_view,
+    staff_only_view,
+)
 from auth.views import login_view, register_view
-from subscriptions.views import subscriptions_price_view, user_subscriptions_view, user_subscriptions_cancel_view
+from subscriptions.views import (
+    subscriptions_price_view,
+    user_subscriptions_view,
+    user_subscriptions_cancel_view,
+)
+from landing.views import landing_page_view
 
 urlpatterns = [
-    path('', home_view, name='home'),
+    path("", landing_page_view, name="home"),
     # path('register/', register_view),
     # path("login/", login_view),
-    path('checkout/sub-price/<int:price_id>/', product_price_redirect_views, name='sub-price-checkout'),
-    path('checkout/start/', checkout_redirect_view, name='stripe-checkout-start'),
-    path('checkout/sucess/', checkout_finalised_view, name='stripe-checkout-end'),
-    path('about/', about_view),
-    path('hello-world/', home_view),
-    path('admin/', admin.site.urls),
-    path('accounts/billing/', user_subscriptions_view, name="user_subscription"),
-    path('accounts/billing/cancel', user_subscriptions_cancel_view, name="user_subscription_cancel"),
-    path('accounts/', include('allauth.urls')),
-    path('protected/', pw_protected_view),
-    path('protected/user-only', user_only_view),
-    path('protected/staff-only', staff_only_view),
-    path('profiles/', include('profiles.urls')),
-    path('pricing/', subscriptions_price_view, name='pricing'),
-    path('pricing/<str:interval>/', subscriptions_price_view, name='pricing'),
+    path(
+        "checkout/sub-price/<int:price_id>/",
+        product_price_redirect_views,
+        name="sub-price-checkout",
+    ),
+    path("checkout/start/", checkout_redirect_view, name="stripe-checkout-start"),
+    path("checkout/sucess/", checkout_finalised_view, name="stripe-checkout-end"),
+    path("about/", about_view),
+    path("hello-world/", home_view),
+    path("admin/", admin.site.urls),
+    path("accounts/billing/", user_subscriptions_view, name="user_subscription"),
+    path(
+        "accounts/billing/cancel",
+        user_subscriptions_cancel_view,
+        name="user_subscription_cancel",
+    ),
+    path("accounts/", include("allauth.urls")),
+    path("protected/", pw_protected_view),
+    path("protected/user-only", user_only_view),
+    path("protected/staff-only", staff_only_view),
+    path("profiles/", include("profiles.urls")),
+    path("pricing/", subscriptions_price_view, name="pricing"),
+    path("pricing/<str:interval>/", subscriptions_price_view, name="pricing"),
 ]
---
+++ src/landing/views.py
@@ -0,0 +1,22 @@
+from helpers.numbers import shorten_number
+from django.shortcuts import render
+
+# Create your views here.
+# from dashboard.views import dashboard_view
+
+from visits.models import PageVisit
+
+
+def landing_page_view(request):
+    qs = PageVisit.objects.all()
+    PageVisit.objects.create(path=request.path)
+    page_views_formatted = shorten_number(qs.count() * 100_000)
+    social_views_formatted = shorten_number(qs.count() * 23_000)
+    return render(
+        request,
+        "landing/main.html",
+        {
+            "page_view_count": page_views_formatted,
+            "social_views_count": social_views_formatted,
+        },
+    )
